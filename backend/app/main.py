30995027d18fee34d16aef7d45e860326f5cf26b
manavlade <manavlade14690@gmail.com>
2026-05-27 11:06:20 +0530

    changed frontend url

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://infer-flow-fmgj.vercel.app"
+        "https://infer-flow-frontend.vercel.app"
     ],
 
     allow_credentials=True,
